659b9e0c2a29243f847ec1f28acf7a2d78587454
surajd <surajd@sdcorp.net>
2023-08-09 10:02:06 +0530

    fixed merged

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 #This is simple code main file
-<<<<<<< HEAD
 editing in this file
-=======
 int(a,b)
 add (a+b)
 
->>>>>>> bb46a90 (changes code in main.py)
--- test/main.py
+++ test/main.py
@@ -1,6 +1,3 @@
 #This is the test file contents unit test cases
-<<<<<<< HEAD
 this is imp file
-=======
 changes in unit test file`
->>>>>>> 5a653f5 (changes in unit test file)
